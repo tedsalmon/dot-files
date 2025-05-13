9bcc8be1bb13c3454da29d95ee36bf99d71330b3
Ted Salmon <tass2001@gmail.com>
2025-05-13 00:14:38 -0400

    [IMP] nvim: Replace bufdelete, indent and tree with snacks.nvim

---
+++ nvim/lua/tsalmon/plugins/snacks.lua
@@ -0,0 +1,32 @@
+return {
+    "folke/snacks.nvim",
+    priority = 1000,
+    lazy = false,
+    keys = {
+        {"<leader>q", function() Snacks.bufdelete() end, desc = "Buffer delete", mode="n"},
+        {"<leader>g", function() Snacks.explorer.reveal() end, desc = "Go To File", mode="n"}
+    },
+    opts = {
+        bigfile = { enabled = true },
+        bufdelete = {
+            enabled = true
+        },
+        explorer = {
+            enabled = true,
+            replace_netrw = true
+        },
+        indent = { enabled = true },
+        input = { enabled = true },
+        picker = {
+            enabled = true,
+            hidden = true,
+            ignored = true
+        },
+        notifier = { enabled = true },
+        quickfile = { enabled = true },
+        scope = { enabled = true },
+        scroll = { enabled = true },
+        statuscolumn = { enabled = true },
+        words = { enabled = true },
+    },
+}
